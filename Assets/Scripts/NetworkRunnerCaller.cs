a432b44f4715ee5c601383caa1f414f4f9bc4ff7
redd096 <martin.babino96@gmail.com>
2022-08-28 16:38:39 +0200

    init network runner caller

--- Assets/Scripts/NetworkRunnerCaller.cs
+++ Assets/Scripts/NetworkRunnerCaller.cs
@@ -1,18 +1,69 @@
-using System.Collections;
+using Fusion;
+using Fusion.Sockets;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NetworkRunnerCaller : MonoBehaviour
+//TODO other ways to despawn player instances + add Nickname and other variables to player
+//https://doc.photonengine.com/en-us/fusion/current/samples/game-samples/fusion-razor-madness
+
+public class NetworkRunnerCaller : MonoBehaviour, INetworkRunnerCallbacks
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private NetworkPrefabRef playerPrefab = default;
+
+    private Dictionary<PlayerRef, NetworkObject> spawnedCharacters = new Dictionary<PlayerRef, NetworkObject>();
+
+    /// <summary>
+    /// When a player join, server must spawn its prefab and save in dictionary
+    /// </summary>
+    /// <param name="runner"></param>
+    /// <param name="player"></param>
+    public void OnPlayerJoined(NetworkRunner runner, PlayerRef player)
     {
-        
+        if (runner.IsServer)
+        {
+            //TODO create various player spawn
+            NetworkObject networkPlayerObject = runner.Spawn(playerPrefab, Vector2.zero, Quaternion.identity, player);
+            spawnedCharacters.Add(player, networkPlayerObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// Whena a player left, server must destroy player instance and remove from dictionary
+    /// </summary>
+    /// <param name="runner"></param>
+    /// <param name="player"></param>
+    public void OnPlayerLeft(NetworkRunner runner, PlayerRef player)
     {
-        
+        if (spawnedCharacters.TryGetValue(player, out NetworkObject networkObject))
+        {
+            runner.Despawn(networkObject);
+            spawnedCharacters.Remove(player);
+        }
     }
+
+    /// <summary>
+    /// Client collect inputs and send it to the server
+    /// </summary>
+    /// <param name="runner"></param>
+    /// <param name="input"></param>
+    public void OnInput(NetworkRunner runner, NetworkInput input) { }
+    public void OnInputMissing(NetworkRunner runner, PlayerRef player, NetworkInput input) { }
+    public void OnShutdown(NetworkRunner runner, ShutdownReason shutdownReason) { }
+    public void OnConnectedToServer(NetworkRunner runner) { }
+    public void OnDisconnectedFromServer(NetworkRunner runner) { }
+    public void OnConnectRequest(NetworkRunner runner, NetworkRunnerCallbackArgs.ConnectRequest request, byte[] token) { }
+    public void OnConnectFailed(NetworkRunner runner, NetAddress remoteAddress, NetConnectFailedReason reason) { }
+    public void OnUserSimulationMessage(NetworkRunner runner, SimulationMessagePtr message) { }
+    public void OnSessionListUpdated(NetworkRunner runner, List<SessionInfo> sessionList) { }
+    public void OnCustomAuthenticationResponse(NetworkRunner runner, Dictionary<string, object> data) { }
+    /// <summary>
+    /// TODO must send player dictionary from host to host
+    /// </summary>
+    /// <param name="runner"></param>
+    /// <param name="hostMigrationToken"></param>
+    public void OnHostMigration(NetworkRunner runner, HostMigrationToken hostMigrationToken) { }
+    public void OnReliableDataReceived(NetworkRunner runner, PlayerRef player, ArraySegment<byte> data) { }
+    public void OnSceneLoadDone(NetworkRunner runner) { }
+    public void OnSceneLoadStart(NetworkRunner runner) { }
 }
